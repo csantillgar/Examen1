833b59e556d86a179ce04961283bd1eb995e1c29
csantillgar <csantill@myuax.com>
2023-11-09 12:32:55 +0100

    comentarios
    comentarios del ejercicio 1

--- Ejercicio1-Operaciones Basicas/main.cpp
+++ Ejercicio1-Operaciones Basicas/main.cpp
@@ -2,15 +2,13 @@
 using namespace std;
 
 int main(){
-    int a,b, resultado;
+    int a,b, resultado;//Declaracion de variables
 
-cout<<"Ingrese el primer numero: "; cin>>a;
-cout<<"Ingrese el segundo numero: "; cin>>b;
-resultado = a+b;
-cout<<"La suma es: "; cout<<resultado;
+cout<<"Ingrese el primer numero: "; cin>>a;//Solicitud del primer valor
+cout<<"Ingrese el segundo numero: "; cin>>b;//Solicitud del segundo valor
+resultado = a+b;//Operacion de suma
+cout<<"La suma es: "; cout<<resultado;//Impresion del resultado
 
-
-
-    return 0;
+    return 0;//Retorno de la funcion main
 }
 
