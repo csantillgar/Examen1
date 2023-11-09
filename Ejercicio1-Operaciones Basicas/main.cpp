2258fbe3be6dba393515da7896d729f35324de1e
csantillgar <csantill@myuax.com>
2023-11-09 12:24:19 +0100

    Ejercicio1

---
+++ Ejercicio1-Operaciones Basicas/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b, resultado;
+
+cout<<"Ingrese el primer numero: "; cin>>a;
+cout<<"Ingrese el segundo numero: "; cin>>b;
+resultado = a+b;
+cout<<"La suma es: "; cout<<resultado;
+
+
+
+    return 0;
+}
+
