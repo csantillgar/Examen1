23f1ea7d100012b852ef9819bf37140cfe7becf5
csantillgar <csantill@myuax.com>
2023-11-09 12:58:26 +0100

    Ejercicio3
    Ejercicio 3 y comentarios3

--- Ejercicio3-Optimizacion de Memoria/main.cpp
+++ Ejercicio3-Optimizacion de Memoria/main.cpp
@@ -1,14 +1,18 @@
 #include<iostream>
 using namespace std;
-void intercambiar_valores(int* a, int* b){
+void intercambiar_valores(int* a, int* b){//Paso por referencia
     int temp = *a;
     *a = *b;
-    *b = temp;
+    *b = temp;//puntero temporal
 }
 int main(){
+int num1 = 7;//Asignacion de valores iniciales
+int num2 = 9;
 
+cout <<"Valores originales: " << num1 << " " << num2 << endl;//Se imprimen los valores originales
 
-
+intercambiar_valores(&num1, &num2);//Intercambio de valores
+cout<<"Valores intercambiados: " << num1 << " " << num2 << endl;//Se imprimen los valores intercambiados
 
     return 0;
 }
