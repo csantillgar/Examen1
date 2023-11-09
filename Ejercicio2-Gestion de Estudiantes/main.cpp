f2586cfedde5c5663e793934f2bf7e78082a3257
csantillgar <csantill@myuax.com>
2023-11-09 12:40:03 +0100

    Ejercicio2

--- Ejercicio2-Gestion de Estudiantes/main.cpp
+++ Ejercicio2-Gestion de Estudiantes/main.cpp
@@ -1,10 +1,25 @@
 #include<iostream>
 using namespace std;
+
+class Estudiante {
+public:
+    string nombre;
+    int edad;
+    string grado;
+
+    void mostrar_info(){
+        cout << "Nombre: " << nombre << endl;
+        cout << "Edad: " << edad << " anos"<< endl;
+        cout << "Grado: " << grado << endl;
+    }
+};
 int main(){
+    Estudiante estudiante1;
+    estudiante1.nombre = "Juan";
+    estudiante1.edad = 15;
+    estudiante1.grado = "Segundo";
 
-
-
-
+    estudiante1.mostrar_info();
 
     return 0;
 }
